3c48ffe4eef35defb8a125545eab367c1fa6a2a4
Ryan <ryanbranch@users.noreply.github.com>
2020-03-23 00:39:24 -0400

    Moving code around
    Moved test code (from the tutorial referenced in previous commit) into main

--- spifer.py
+++ spifer.py
@@ -9,19 +9,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})')
-
-client.run(TOKEN)
-
 """
 _________________________________________________
 #TODO
@@ -76,7 +63,19 @@ def main():
     # Initializes the random number generator
     random.seed("333   333   333")
     spider = ServerSpider()
+
+    client = discord.Client()
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+        print(f'{client.user} is connected to the following guild:\n'
+              f'{guild.name}(id: {guild.id})')
+    client.run(TOKEN)
+
     spider.crawl()
+
     return 0
 
 
